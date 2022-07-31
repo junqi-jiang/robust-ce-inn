d5b0af696cc1ed494da95856b10c6b2de9774732
Jay Jiang <jiangjunqi1998@gmail.com>
2022-07-31 13:02:04 +0100

    Linear constraint stable, verified correctness in heloc.ipynb

--- optsolver.py
+++ optsolver.py
@@ -27,6 +27,7 @@ class OptSolver:
                 for var_idx in self.dataset.feat_var_map[feat_idx]:
                     node_var[var_idx] = self.model.addVar(vtype=GRB.BINARY, name='x_disc_0_' + str(var_idx), ub=1, lb=0)
                     disc_var_list.append(node_var[var_idx])
+                    #self.model.addConstr(node_var[var_idx] == self.x[var_idx])  ### debug
                 self.model.update()
                 self.model.addConstr(quicksum(disc_var_list) == 1, name='x_disc_0_feat' + str(feat_idx))
 
@@ -36,6 +37,7 @@ class OptSolver:
                 for i, var_idx in enumerate(self.dataset.feat_var_map[feat_idx]):
                     node_var[var_idx] = self.model.addVar(vtype=GRB.BINARY, name='x_ord_0_' + str(var_idx), ub=1, lb=0)
                     self.model.update()
+                    #self.model.addConstr(node_var[var_idx] == self.x[var_idx])      ### debug
                     if i != 0:
                         self.model.addConstr(prev_var >= node_var[var_idx],
                                              name='x_ord_0_var' + str(var_idx - 1) + '_geq_' + str(var_idx))
@@ -45,8 +47,8 @@ class OptSolver:
 
             if self.dataset.feature_types[feat_idx] == DataType.CONTINUOUS_REAL:
                 var_idx = self.dataset.feat_var_map[feat_idx][0]
-                node_var[var_idx] = self.model.addVar(lb=0, ub=1, vtype=GRB.CONTINUOUS, name="x_cont_0_" + str(var_idx))
-
+                node_var[var_idx] = self.model.addVar(lb=0, ub=1, vtype=GRB.SEMICONT, name="x_cont_0_" + str(var_idx))
+                #self.model.addConstr(node_var[var_idx] == self.x[var_idx])  ### debug
             self.model.update()
         return node_var
 
@@ -68,8 +70,8 @@ class OptSolver:
                 self.model.update()
                 # add auxiliary variable for hidden layer nodes
                 if i != (self.inn.num_layers - 1):
-                    node_var[node.index] = self.model.addVar(lb=0, vtype=GRB.CONTINUOUS, name='n_' + str(node))
-                    aux_var[node.index] = self.model.addVar(lb=-GRB.INFINITY, vtype=GRB.CONTINUOUS,
+                    node_var[node.index] = self.model.addVar(lb=0, vtype=GRB.SEMICONT, name='n_' + str(node))
+                    aux_var[node.index] = self.model.addVar(lb=-GRB.INFINITY, vtype=GRB.SEMICONT,
                                                             name='a_' + str(node))
                     self.model.update()
                     # node1 are nodes in prev layer
@@ -82,17 +84,19 @@ class OptSolver:
 
                 # add output constraint: y = WX+b,
                 else:
-                    node_var[node.index] = self.model.addVar(lb=-GRB.INFINITY, vtype=GRB.CONTINUOUS,
+                    node_var[node.index] = self.model.addVar(lb=-GRB.INFINITY, vtype=GRB.SEMICONT,
                                                              name='n_' + str(node))
                     self.model.addConstr(node_var[node.index] == quicksum(
                         (self.inn.weights[(node1, node)].get_bound(self.y_prime) * node_vars[i - 1][node1.index]) for
                         node1 in self.inn.nodes[i - 1]) + self.inn.biases[node].get_bound(self.y_prime),
                                          name="forward_pass_output_node_" + str(node))
                     # add robust counterfactual constraint:
+                         ### debug
                     if self.y_prime:
                         self.model.addConstr(node_var[node.index] - self.eps >= 0.0, name="output_node_lb_>=0")
                     else:
                         self.model.addConstr(node_var[node.index] + self.eps <= 0.0, name="output_node_ub_<0")
+
                     self.model.update()
             node_vars[i] = node_var
             if i != (self.inn.num_layers - 1):
@@ -109,7 +113,7 @@ class OptSolver:
     def set_objective(self, node_vars):
         obj_vars = []
         for idx in node_vars[0].keys():
-            this_obj_var = self.model.addVar(vtype=GRB.CONTINUOUS, lb=0, ub=1, name=f"obj_xd_{idx}")
+            this_obj_var = self.model.addVar(vtype=GRB.SEMICONT, lb=0, ub=1, name=f"obj_xd_{idx}")
             self.model.update()
             self.model.addConstr(this_obj_var >= (self.x[idx] - node_vars[0][idx]), name="objective1")
             self.model.addConstr(this_obj_var >= (node_vars[0][idx] - self.x[idx]), name="objective2")
@@ -120,17 +124,17 @@ class OptSolver:
 
     def set_objective_l1_l0(self, node_vars):
         obj_vars_l1 = []  # each is l1 distance for 1 feature
-        obj_vars_l0 = []  # each is l0 distance for 1 feature
+        #obj_vars_l0 = []  # each is l0 distance for 1 feature
         for feat_idx in range(self.dataset.num_features):
             self.model.update()
-            this_obj_var_l1 = self.model.addVar(vtype=GRB.CONTINUOUS, lb=-GRB.INFINITY, name=f"objl1_feat_{feat_idx}")
-            this_obj_var_l0 = self.model.addVar(vtype=GRB.BINARY, name=f"objl0_feat_{feat_idx}")
+            this_obj_var_l1 = self.model.addVar(vtype=GRB.SEMICONT, lb=-GRB.INFINITY, name=f"objl1_feat_{feat_idx}")
+            #this_obj_var_l0 = self.model.addVar(vtype=GRB.BINARY, name=f"objl0_feat_{feat_idx}")
             var_idxs = self.dataset.feat_var_map[feat_idx]
 
             if self.dataset.feature_types[feat_idx] == DataType.DISCRETE:
                 to_max = dict()
                 for var_idx in var_idxs:
-                    to_max[var_idx] = self.model.addVar(vtype=GRB.CONTINUOUS, lb=-GRB.INFINITY,
+                    to_max[var_idx] = self.model.addVar(vtype=GRB.SEMICONT, lb=-GRB.INFINITY,
                                                         name=f"objl1_feat_disc_{var_idx}")
                     self.model.update()
                     self.model.addConstr(to_max[var_idx] >= (node_vars[0][var_idx] - self.x[var_idx]),
@@ -140,8 +144,8 @@ class OptSolver:
                     self.model.update()
                 self.model.addConstr(this_obj_var_l1 == max_([to_max[idx] for idx in to_max.keys()]),
                                      name=f"objl1_feat_{feat_idx}")
-                self.model.addConstr((this_obj_var_l0 == 0) >> (this_obj_var_l1 <= self.eps),
-                                     name=f"objl0_feat_{feat_idx}")  # if l1<= 0, l0=0
+                #self.model.addConstr((this_obj_var_l0 == 0) >> (this_obj_var_l1 <= self.eps),
+                #                     name=f"objl0_feat_{feat_idx}")  # if l1<= 0, l0=0
 
             if self.dataset.feature_types[feat_idx] == DataType.ORDINAL:
                 self.model.addConstr(this_obj_var_l1 >= (
@@ -150,8 +154,8 @@ class OptSolver:
                 self.model.addConstr(this_obj_var_l1 >= (
                             np.sum(self.x[var_idxs]) - quicksum([node_vars[0][idx] for idx in var_idxs])) / (
                                              len(var_idxs) - 1), name=f"objl1_feat_{feat_idx}_>=+")     # for abs()
-                self.model.addConstr((this_obj_var_l0 == 0) >> (this_obj_var_l1 <= self.eps),
-                                     name=f"objl0_feat_{feat_idx}")
+                #self.model.addConstr((this_obj_var_l0 == 0) >> (this_obj_var_l1 <= self.eps),
+                #                     name=f"objl0_feat_{feat_idx}")
 
             if self.dataset.feature_types[feat_idx] == DataType.CONTINUOUS_REAL:
                 self.model.addConstr(
@@ -160,11 +164,11 @@ class OptSolver:
                 self.model.addConstr(
                     this_obj_var_l1 >= (np.sum(self.x[var_idxs]) - quicksum([node_vars[0][idx] for idx in var_idxs])),
                     name=f"objl1_feat_{feat_idx}_>=+")      # for abs()
-                self.model.addConstr((this_obj_var_l0 == 0) >> (this_obj_var_l1 <= self.eps),
-                                     name=f"objl0_feat_{feat_idx}")
+                #self.model.addConstr((this_obj_var_l0 == 0) >> (this_obj_var_l1 <= self.eps),
+                #                     name=f"objl0_feat_{feat_idx}")
 
             obj_vars_l1.append(this_obj_var_l1)
-            obj_vars_l0.append(this_obj_var_l0)
+            #obj_vars_l0.append(this_obj_var_l0)
 
         #self.model.setObjective(
         #    quicksum(obj_vars_l1) / self.dataset.num_features + quicksum(obj_vars_l0) / self.dataset.num_features,
@@ -174,7 +178,7 @@ class OptSolver:
 
     def compute(self):
         node_vars, aux_vars = self.create_constraints()
-        self.set_objective_l1_l0(node_vars)
+        self.set_objective_l1_l0(node_vars)    ### debug
         self.model.Params.LogToConsole = 0  # disable console output
         self.model.optimize()
         xp = []
